6f435541f6893a50ff2370824e85e32835836e75
Pooya Parsa <pyapar@gmail.com>
2021-01-11 21:31:09 +0100

    chore: move components to runtime

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -2,5 +2,5 @@ export * from './image'
 export * from './utils'
 export * from './observer'
 
-export { default as NuxtImg } from '../components/nuxt-img.vue'
-export { default as NuxtPicture } from '../components/nuxt-picture.vue'
+export { default as NuxtImg } from './components/nuxt-img.vue'
+export { default as NuxtPicture } from './components/nuxt-picture.vue'
