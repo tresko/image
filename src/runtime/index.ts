d2652d7475453cfc4a0461c521f82946def97170
Pooya Parsa <pyapar@gmail.com>
2021-01-11 21:55:14 +0100

    chore(plg): update runtime bundling

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,6 +1,3 @@
 export * from './image'
 export * from './utils'
 export * from './observer'
-
-export { default as NuxtImg } from './components/nuxt-img.vue'
-export { default as NuxtPicture } from './components/nuxt-picture.vue'
